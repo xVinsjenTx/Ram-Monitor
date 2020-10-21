1670cecf1c04ad9a973227ac9d904d3a40c39e3c
xVinsjenTx <42538217+xVinsjenTx@users.noreply.github.com>
2020-10-21 13:39:10 +0200

    Update ram_monitor.sh
    Syntex error on line 42. Can't find out why.

--- ram_monitor.sh
+++ ram_monitor.sh
@@ -1,6 +1,12 @@
+#@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 main(){
+    #fetch total memory
+    TOTAL="$(free --mega | awk 'NR==2{print $2}')"
+
+    #set threshold to 80%
+    THRESHOLD="$(awk -v a="${TOTAL}" -v b=0.8 'BEGIN{c=(a*b); print c;}' | awk '{printf "%.0f\n", $1}')"
     #Fetch amount of memory
     MEMAMOUNT="$(free -m | awk '/Mem/{print $2}')"
    
@@ -8,22 +14,22 @@ main(){
     THRESHOLD=80
 
     #fetch the process with the highest memory usage
-    HIGHEST_PROCESS="$(pgrep -v COMMAND | awk '{print $4,$11}' | sort -rnk 4 | head -1)"
-
-    #fetch the memory used by the process
-    PROCESS_MEMORY="$(printf "%s\\n" "${HIGHEST_PROCESS}" | awk '{print $1}')"
-
-    #fetch the name of the process
-    PROCESS_NAME="$(printf "%s\\n" "${HIGHEST_PROCESS}" | awk '{print $2}' | sed 's!.*/!!')"
-
-    #set interval (seconds)
+    HIGHEST_PROCESS="$(ps aux | grep -v COMMAND | awk '{print $4,$11}' | sort -rnk 4 | head -1)"
+#@@ -20,18 +20,19 @@ 
+main(){
     INTERVAL=30
 
     while true; do 
-    #Fetch used memory
+        #fetch used memory
+        USED="$(free --mega | awk 'NR==2{print $3}')"
+
+        #compare $USED with $THRESHOLD and continue
+        if (( USED > THRESHOLD )); then
+            notify-send -i dialog-warning "WARNING: High Memory Usage: ${USED} MB / ${TOTAL} MB used." 
+    #Fetch used memory"
     USED="$(free -m | awk '/Mem/{print $3}')"
     TOTALUSED="$(free -m | awk '/Mem/{printf("Ram Usage: %i\n")$3/$2*100}' | awk '{print $3}')"    
-    
+
         #compare $TOTALUSED with $THRESHOLD and continue
         if (( TOTALUSED > THRESHOLD )); then
             notify-send -i dialog-warning "WARNING: High Memory Usage: $USED MB / $MEMAMOUNT MB used. 
@@ -35,5 +41,3 @@ main(){
         sleep "$INTERVAL"
     done
 }
-
-main "$@"
