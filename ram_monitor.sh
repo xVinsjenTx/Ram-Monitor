589a9a18c5ff0d3010f67433dd9b92eeb32a8564
xVinsjenTx <42538217+xVinsjenTx@users.noreply.github.com>
2020-10-22 01:51:58 +0200

    Further Refactor
    Fixed "highest process" not working.
    Fixed "total memory" not working.
    Fixed some errors with the previous refactor.

--- ram_monitor.sh
+++ ram_monitor.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 main(){
-    #fetch total memory
-    TOTAL="$(free --mega | awk 'NR==2{print $2}')"
-
-    #set threshold to 80%
-    THRESHOLD="$(awk -v a="${TOTAL}" -v b=0.8 'BEGIN{c=(a*b); print c;}' | awk '{printf "%.0f\n", $1}')"
+    #Fetch amount of memory
+    MEMAMOUNT="$(free -m | awk '/Mem/{print $2}')"
+   
+    #set memory usage threshold to 80%
+    THRESHOLD=80
 
     #fetch the process with the highest memory usage
-    HIGHEST_PROCESS="$(pgrep -v COMMAND | awk '{print $4,$11}' | sort -rnk 4 | head -1)"
+    HIGHEST_PROCESS="$(ps aux | grep -v COMMAND | awk '{print $4,$11}' | sort -rnk 4 | head -1)"
 
     #fetch the memory used by the process
     PROCESS_MEMORY="$(printf "%s\\n" "${HIGHEST_PROCESS}" | awk '{print $1}')"
@@ -20,18 +20,19 @@ main(){
     INTERVAL=30
 
     while true; do 
-        #fetch used memory
-        USED="$(free --mega | awk 'NR==2{print $3}')"
-
-        #compare $USED with $THRESHOLD and continue
-        if (( USED > THRESHOLD )); then
-            notify-send -i dialog-warning "WARNING: High Memory Usage: ${USED} MB / ${TOTAL} MB used. 
+    #Fetch used memory
+    USED="$(free -m | awk '/Mem/{print $3}')"
+    TOTALUSED="$(free -m | awk '/Mem/{print $3/$2*100}' | awk '{printf "%.0f\n", $1}')"
+    
+        #compare $TOTALUSED with $THRESHOLD and continue
+        if (( $TOTALUSED > $THRESHOLD )); then
+            notify-send -i dialog-warning "WARNING: High Memory Usage: $USED MB / $MEMAMOUNT MB used. 
             Highest process: ${PROCESS_NAME} uses: ${PROCESS_MEMORY} %"
         else
-            printf "Memory Usage: %s MB / %s MB used\\n" "${USED}" "${TOTAL}"
+            printf "Memory Usage: %s MB / %s MB used\\n" "${USED}" "${MEMAMOUNT}"
         fi
 
-        sleep $INTERVAL
+        sleep "$INTERVAL"
     done
 }
 
